c42dfb8211e5bc1c0f0ad0806046077fb66d8eae
ArmaniLeSure <54874404+ArmaniLeSure@users.noreply.github.com>
2020-04-21 20:22:08 -0500

    map part 2

--- googlemapjs.js
+++ googlemapjs.js
@@ -1,5 +1,5 @@
 function init(){
-	var el = document.getElementById('map');
+	var el = new google.maps.Map(document.getElementById('map'));
 	var myLocation = new google.maps.LatLng(41.833938,-87.626078);
 	var mapOptions = {
 		center: myLocation,
